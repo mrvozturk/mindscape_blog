e0888eb2332400e6392e3bcb7e483c2619a2f47a
Merve Ozturk <ozturkmerve745@gmail.com>
2024-01-29 16:26:34 +0300

    update about

--- src/components/aboutversion.js
+++ src/components/aboutversion.js
@@ -6,12 +6,18 @@ const AboutPage = () => {
   return (
     <div className='custom-page-container'>
       <div className='custom-text-container'>
-        <div className='custom-image-container'>
+        <div className='custom-image-container animated-image'>
           <img src={aboutImage} alt='About' className='custom-image' />
         </div>
-        <div className='custom-paragraph'>
+        <div className='custom-paragraph animated-text'>
+          <h1 className='custom-about'>About Me</h1>
           <p>
-            Ability to put themselves in the merchant's shoes. It is meant to partner on the long run and work as an extension of the merchant's team. A digital agency is a business you hire to outsource your digital marketing efforts, instead of handling in-house. They can provide your business with a variety of digital solutions to promote your product or service online and help you.
+            Ability to put themselves in the merchant's shoes. It is meant to
+            partner on the long run and work as an extension of the merchant's
+            team. A digital agency is a business you hire to outsource your
+            digital marketing efforts, instead of handling in-house. They can
+            provide your business with a variety of digital solutions to promote
+            your product or service online and help you.
           </p>
         </div>
       </div>
